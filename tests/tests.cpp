3692fb5c2b59beb015caccf8aa7532497903178d
Fyrmon Warslay <fyrmonwarslay@gmail.com>
2025-11-01 23:30:29 +0100

    Add tests for operator[] and at()

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -9,11 +9,25 @@
 
 };
 
-TEST_F(DynamicArrayTest ,ConstructingWithSize)
+TEST_F(DynamicArrayTest, Indexing)
+{
+    EXPECT_EQ(testArr[1],2);
+    EXPECT_EQ(testArr2[0],1);
+}
+
+TEST_F(DynamicArrayTest, At)
+{
+    EXPECT_EQ(testArr.at(1),2);
+    EXPECT_EQ(testArr2.at(0),1);
+}
+
+TEST_F(DynamicArrayTest ,Size)
 {
     EXPECT_EQ(testArr.size(),3);
     EXPECT_EQ(testArr2.size(),1);
 }
+
+
 TEST_F(DynamicArrayTest, Capacity)
 {
     EXPECT_EQ(testArr.capacity(), 3);
