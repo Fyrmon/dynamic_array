4c04c5ac14a1cf15012acd70e1da6cb02bd9adb8
Fyrmon Warslay <fyrmonwarslay@gmail.com>
2025-11-10 19:23:45 +0100

    Implement: swap(other)

--- app/main.cpp
+++ app/main.cpp
@@ -4,8 +4,11 @@
 int main()
 {
     DynamicArray<int> arr{1,2,3,4,5,6};
+    DynamicArray<int> arr2{7,8,9};
+
     
-    arr.pop_back();
-    std::cout<<arr<<'\n';
+    arr.swap(arr2);
+    std::cout<<"Arr:"<<arr<<"\nWith cap: "<<arr.capacity()<<" and size: "<<arr.size()<<'\n';
+    std::cout<<"Arr2:"<<arr2<<"\nWith cap: "<<arr2.capacity()<<" and size: "<<arr2.size()<<'\n';
     return 0;
 }
